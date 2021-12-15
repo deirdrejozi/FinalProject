323e154d7b255b3867e7f741d7310b4301301f9c
deirdrejozi <92895331+deirdrejozi@users.noreply.github.com>
2021-12-15 16:09:38 -0600

    Update Drama.java

--- EasternDramas/src/main/java/edu/dmacc/entities/Drama.java
+++ EasternDramas/src/main/java/edu/dmacc/entities/Drama.java
@@ -1,42 +1,61 @@
-package edu.dmacc.entities;
+package dmacc.drama.entities;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.math.BigDecimal;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author Pamela D. Jozi
  * Date: 12/10/2021
  * Time: 11:59
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-@NoArgsConstructor
-@Entity
-@Table(name="Drama")
-public class Drama {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer dramaId;
+public class Drama extends Node implements Comparable<Drama> {
+    private static int nextId = 1;
+    private int dramaId;
     private String dramaTitle;
     private int numOfReviews;
-    private BigDecimal revStars;
+    private double revStars;
     private int releaseYear;
-    private boolean subtitled;
+    private String country;
+    private String language;
+    private String subtitles;
+    private String genre;
+    private String rating;
 
-/*
-    private Integer primaryLang;
-    private Integer subtitlesLang;
+    public Drama(String dramaTitle, int numOfReviews, double revStars, int releaseYear,
+                 String subtitles, String language, String country, String genre, String rating) {
+        super(AttrType.DRAMA);
+        this.dramaId = nextId++;
+        this.dramaTitle = dramaTitle;
+        this.numOfReviews = numOfReviews;
+        this.revStars = revStars;
+        this.releaseYear = releaseYear;
+        this.subtitles = subtitles;
+        this.language = language;
+        this.country = country;
+        this.genre = genre;
+        this.rating = rating;
+    }
 
-    private Integer country;
-    private Integer genre;
-    private Integer rating;
-*/
+    public static void sort(Drama[] array) {
+        int n = array.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                Drama key = array[i];
+                int j = i;
+                while (j >= gap && array[j - gap].compareTo(key) > 0) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+                array[j] = key;
+            }
+        }
+    }
 
-    private String primaryLanguageName;
-    private String countryName;
-    private String genreName;
-    private String ratingRate;
-
+    @Override
+    public int compareTo(Drama o) {
+        return this.dramaTitle.compareTo(o.dramaTitle);
+    }
 }
+// last edit weds Dec 15 4:10 pm
